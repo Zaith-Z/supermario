3fd247f78803294dbc1d5448c22001b210e2288c
Zaith-Z <84807156+Zaith-Z@users.noreply.github.com>
2026-07-09 13:00:29 +0200

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-if [ "$MARIO_PINK" = "true" ]; then
-  cp /usr/share/nginx/html/images/pink.png /usr/share/nginx/html/images/smallmariosheet.png
-fi
-
-exec /docker-entrypoint.sh nginx -g "daemon off;"
